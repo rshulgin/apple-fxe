de31808a41d47e1ce1aeb940d89614d6f80e59b1
Shulgin Roman <roman@shulgin.pro>
2023-03-16 15:23:37 +0100

    add arrow before price

--- src/components/Header/AaplInfo.tsx
+++ src/components/Header/AaplInfo.tsx
@@ -1,5 +1,7 @@
 import React, {memo} from "react";
 import {Box, Typography} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 
 type AaplInfoProps = {
     price?: number;
@@ -7,16 +9,29 @@ type AaplInfoProps = {
     percentChange?: number;
 }
 const changePreformat = new Intl.NumberFormat('en-US', {signDisplay: 'always'});
-export const AaplInfo = memo(({price, change, percentChange}: AaplInfoProps) => (
+const iconStyles = {
+    fontSize: "4rem",
+    marginLeft: '-1rem',
+}
+export const AaplInfo = memo(({price, change, percentChange}: AaplInfoProps) => {
+    const isUp = change && change >= 0;
+    return (
         <Box>
-            <Typography variant={"h3"} fontSize='4rem' lineHeight={1}>
-                {price}
-            </Typography>
+            <Box display="flex">
+                <Typography color={ isUp ? '#388e3c' : 'rgb(217, 30, 24)'} width="2rem" lineHeight={1}>
+                    {isUp ? <ArrowDropUpIcon sx={iconStyles} /> : <ArrowDropDownIcon sx={iconStyles}/>}
+                </Typography>
+                <Typography variant={"h3"} fontSize='4rem' lineHeight={1}>
+                    {price}
+                </Typography>
+            </Box>
+
             <Box display={'flex'} alignItems={'end'} justifyContent={'space-between'}
-                color={change && change > 0 ? '#388e3c' : 'rgb(217, 30, 24)'}
+                 color={ isUp ? '#388e3c' : 'rgb(217, 30, 24)'}
             >
                 <Typography variant={"h5"}>{change ? changePreformat.format(change) : '--.--'}</Typography>
                 <Typography variant={"h5"}>({percentChange ? changePreformat.format(percentChange) : '--.--'}%)</Typography>
             </Box>
         </Box>
-))
+    );
+})
